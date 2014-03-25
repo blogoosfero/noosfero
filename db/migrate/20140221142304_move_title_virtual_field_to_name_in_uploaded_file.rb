8e135eac388605969f8378a02969f0bb7ecdc38e
Braulio Bhavamitra <braulio@eita.org.br>
2014-03-25 18:57:13 +0000

    Protect against non UTF8 chars

--- db/migrate/20140221142304_move_title_virtual_field_to_name_in_uploaded_file.rb
+++ db/migrate/20140221142304_move_title_virtual_field_to_name_in_uploaded_file.rb
@@ -1,7 +1,8 @@
 class MoveTitleVirtualFieldToNameInUploadedFile < ActiveRecord::Migration
   def self.up
+    i = Iconv.new 'UTF-8//IGNORE', 'UTF-8'
     UploadedFile.find_each do |uploaded_file|
-      uploaded_file.name = uploaded_file.setting.delete :title
+      uploaded_file.name = i.iconv uploaded_file.setting.delete(:title)
       uploaded_file.send :update_without_callbacks
     end
   end
