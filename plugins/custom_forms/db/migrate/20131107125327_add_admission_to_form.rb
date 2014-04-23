39ca5ffc96537dd152b02df73fc3703d70583a7a
Braulio Bhavamitra <braulio@eita.org.br>
2014-04-23 13:56:49 -0300

    Continue after errors

--- plugins/custom_forms/db/migrate/20131107125327_add_admission_to_form.rb
+++ plugins/custom_forms/db/migrate/20131107125327_add_admission_to_form.rb
@@ -2,17 +2,17 @@ class AddAdmissionToForm < ActiveRecord::Migration
   def self.up
     change_table :custom_forms_plugin_forms do |t|
       t.boolean :for_admission, :default => false
-    end 
+    end
 
     CustomFormsPlugin::Form.find_each do |f|
       f.for_admission = false
-      f.save!
+      f.send :update_without_callbacks
     end
   end
 
   def self.down
     change_table :custom_forms_plugin_forms do |t|
       t.remove :for_admission
-    end 
+    end
   end
 end
