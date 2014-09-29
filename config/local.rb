30f74c5ca15e19df51f30f9f7ef4321ed11b288a
Braulio Bhavamitra <braulio@eita.org.br>
2014-09-29 16:16:20 -0300

    Set timezone

--- config/local.rb
+++ config/local.rb
@@ -1,5 +1,6 @@
 Noosfero.default_locale = 'pt'
 FastGettext.locale = Noosfero.default_locale
 I18n.locale = Noosfero.default_locale
+Time.zone = 'America/Sao_Paulo'
 
 ActionMailer::Base.delivery_method = :sendmail
