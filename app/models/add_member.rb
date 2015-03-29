d248cbca10a448b9144418f48a5b1594e9d29285
Braulio Bhavamitra <braulio@eita.org.br>
2015-03-29 00:06:53 -0300

    add_member: fix information

--- app/models/add_member.rb
+++ app/models/add_member.rb
@@ -22,7 +22,8 @@ class AddMember < Task
   end
 
   def information
-    {:message => _("%{requestor} wants to be a member of '%{organization}'.")}
+    {:message => _("%{requestor} wants to be a member of '%{organization}'."),
+     variables: {requestor: requestor.name, organization: organization.name}}
   end
 
   def accept_details
