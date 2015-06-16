4872589e32ff5c4c43ae941ae6c71ae11987951d
Braulio Bhavamitra <braulio@eita.org.br>
2015-06-16 15:33:33 -0300

    diagramo: fix call of user

--- plugins/diagramo/lib/diagramo_plugin.rb
+++ plugins/diagramo/lib/diagramo_plugin.rb
@@ -17,7 +17,7 @@ class DiagramoPlugin < Noosfero::Plugin
   end
 
   def article_toolbar_actions article
-    user = context.user
+    user = context.send :user
     return unless user and user.is_admin? environment
     return unless article.folder?
     lambda do
